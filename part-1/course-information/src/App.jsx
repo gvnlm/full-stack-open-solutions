1f88226a371725bb29bab627f1e8a83a66d1583d
Gavin Lim <150659730+gvnlm@users.noreply.github.com>
2024-09-14 16:42:42 +1000

    Complete step 5 of Course Information app front-end

--- part-1/course-information/src/App.jsx
+++ part-1/course-information/src/App.jsx
@@ -1,25 +1,27 @@
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
 
 
-const Header = ({ course }) => <h1>{course}</h1>;
+const Header = ({ course: { name } }) => <h1>{name}</h1>;
 
 
-const Content = ({ parts }) => (
+const Content = ({ course: { parts } }) => (
   <>
     <Part part={parts[0]} />
     <Part part={parts[1]} />
@@ -28,12 +30,14 @@ const Content = ({ parts }) => (
 );
 
 
-const Total = ({ parts }) => {
+const Total = ({ course: { parts } }) => {
   const total = parts.reduce((sum, { exercises }) => sum + exercises, 0);
   return <p>Number of exercises {total}</p>;
 };
 
 
+// Styling: Since all properties of the path object are utilised, I decided 
+// against destructuring it (i.e., part: { name, exercises }).
 const Part = ({ part }) => <p>{part.name} {part.exercises}</p>;
 
 
