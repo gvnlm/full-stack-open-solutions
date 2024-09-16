f399d1082fc298f83eb303ee1500e106acec7a63
Gavin Lim <150659730+gvnlm@users.noreply.github.com>
2024-09-16 19:16:20 +1000

    Remove whitespace padding from array destructuring syntax

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 
 
 const App = () => {
-  const [ good, setGood ] = useState(0);
-  const [ neutral, setNeutral ] = useState(0);
-  const [ bad, setBad ] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
